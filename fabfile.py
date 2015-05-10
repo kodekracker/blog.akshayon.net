e6256f784513027c3ee1966e0c40020f9f3866a0
Akshay Pratap Singh <pratapakshay0@gmail.com>
2015-05-10 10:50:50 +0530

    (fixed): fix branch checkout by adding .gitignore in output/

--- fabfile.py
+++ fabfile.py
@@ -150,7 +150,7 @@ def publish_github(publish_drafts=False, dns=None):
 
     except Exception:
         pass
-
+    local('cp .gitignore output/')
     local('ghp-import -m "(updated): site updated" output')
     local('git push origin gh-pages:master')
 
